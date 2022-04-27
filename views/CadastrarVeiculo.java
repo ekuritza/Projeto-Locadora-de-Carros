0cdf4455f26d5e669e7a6e06b63b0492a9cb652f
Eduardo Kuritza <81322704+ekuritza@users.noreply.github.com>
2022-04-27 09:24:48 -0300

    Feat/adiciona carro (#1)
    * adcionei classe de carro
    
    * feat/add models carro
    
    * feat/add mvc type
    
    * refactor/take out update car
    
    * refactor/arraylist is ok
    
    * refactor/change code

---
+++ views/CadastrarVeiculo.java
@@ -0,0 +1,24 @@
+package views;
+
+import utils.Console;
+import controllers.VeiculoController;
+import models.Veiculo;
+
+public class CadastrarVeiculo{
+   public void renderizar() {
+    Veiculo veiculo = new Veiculo();
+    VeiculoController veiculoController = new VeiculoController();
+
+    System.out.println("\n -- CADASTRO DE VEÍCULOS -- \n");
+    veiculo.setNome(Console.readString("Digite o nome do veículo: "));
+    veiculo.setFabricante(Console.readString("Digite o fabricante do veículo: "));
+    veiculo.setPlacaDoVeiculo(Console.readString("Digite a placa do veículo: "));
+    veiculo.setCor(Console.readString("Digite a cor do veículo: "));
+    veiculo.setSituacaoDoVeiculo(Console.readBoolean("Digite a situação do veículo (true ou false): "));
+    veiculo.setAnoDoModelo(Console.readString("Digite o ano de fabricação do veículo: "));
+    veiculo.setKm(Console.readString("Informe a quilometragem do veículo: "));
+    veiculo.setValorDiaria(Console.readDouble("Informe o valor da diária: "));
+    veiculoController.cadastrar(veiculo);
+    System.out.println("----------------------------");
+   } 
+}
---
+++ views/Principal.java
@@ -0,0 +1,42 @@
+package views;
+
+import utils.Console;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        System.out.println("-----ALUGUEL DE CARROS UP-----");
+        int escolhaMenu;
+        do {
+            System.out.println("Selecione a opção desejada: ");
+            System.out.println("1  Cadastrar veículo para locação");
+            System.out.println("2  Lista de veículos cadastrados");
+            System.out.println("3  Novo cliente");
+            System.out.println("4  Lista de clientes");
+            System.out.println("5  Cadastro de vendedor");
+            System.out.println("6  Lista de vendedores");
+            System.out.println("7  Cadastro de locação");
+            System.out.println("8  Lista de locações");
+            System.out.println("0  Sair");
+            System.out.println("------------------------------");
+
+            escolhaMenu = Console.readInt("Selecione a opção desejada: ");
+            switch (escolhaMenu) {
+                case 1:
+                    CadastrarVeiculo cadastro = new CadastrarVeiculo();
+                    cadastro.renderizar();
+                    break;
+                case 2:
+                    ListarVeiculo listarVeiculos = new ListarVeiculo();
+                    listarVeiculos.renderizar();
+                    break;
+                case 0:
+                    System.out.println("\n -- SAINDO -- \n");
+                    break;
+                default:
+                    System.out.println("\n -- OPÇÃO NÃO ENCONTRADA -- \n");
+                    break;
+            }
+        } while (escolhaMenu != 0);
+    }
+}
