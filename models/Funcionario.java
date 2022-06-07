3ba51f25c957da45c6d140860f18051a487b1b98
Lobster-dev <80717591+Lobster-dev@users.noreply.github.com>
2022-06-07 10:17:27 -0300

    refactor: Move methods getCpf and setCpf

--- models/Funcionario.java
+++ models/Funcionario.java
@@ -8,14 +8,15 @@ public class Funcionario {
     public String getNome() {
         return nome;
     }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
     public String getCpf() {
         return cpf;
     }
     public void setCpf(String cpf) {
         this.cpf = cpf;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     @Override
